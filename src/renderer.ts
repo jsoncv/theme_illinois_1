ad999ccee0253d81074c982df672ecb089bcf359
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-01 17:23:56 +0300

    Progress Commit cli#4

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,10 +2,13 @@ const Handlebars = require("handlebars")
 import fs from 'fs'
 
 const render = (cv:any) => {
-    const tpl = fs.readFileSync(__dirname + '/../templates/main.hbs', 'utf-8');
-    const templateScript = Handlebars.compile(tpl)
+    const mainTemplate = fs.readFileSync(__dirname + '/../templates/main.hbs', 'utf-8')
+    const style = fs.readFileSync(__dirname + '/../templates/style.css', 'utf-8')
+    const templateScript = Handlebars.compile(mainTemplate)
+    // TODO: Add Partials as well
     return templateScript({
-        cv: cv
+        cv: cv,
+        css: style
     })
 }
 
