b73949d7725cb200462fb2d5949974878d0b5115
Amin Zabardsat <zabardast.amin@metu.edu.tr>
2021-11-02 00:54:46 +0300

    In Progress cli #4

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,11 +1,21 @@
 const Handlebars = require("handlebars")
 import fs from 'fs'
 
-const render = (cv:any) => {
+const render = (cv:any):string => {
     const mainTemplate = fs.readFileSync(__dirname + '/../templates/main.hbs', 'utf-8')
     const style = fs.readFileSync(__dirname + '/../templates/style.css', 'utf-8')
+    const partialsDir = fs.readdirSync(__dirname + '/../templates/partials')
+    partialsDir.forEach(function (filename:string) {
+        const matches = /^([^.]+).hbs$/.exec(filename)
+        if (!matches) {
+            return
+        }
+        const name = matches[1];
+        const filePath = `/../templates/partials/${filename}`
+        const template = fs.readFileSync(__dirname + filePath, 'utf8')
+        Handlebars.registerPartial(name, template)
+    })
     const templateScript = Handlebars.compile(mainTemplate)
-    // TODO: Add Partials as well
     return templateScript({
         cv: cv,
         css: style
